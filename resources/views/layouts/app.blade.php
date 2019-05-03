966b907d7add9f72839b524f7aeeb331900f57c8
Viktoriia Samokysh <viktoriia.samokysh@codeit.com.ua>
2019-05-03 19:46:18 +0300

    Add template view

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,65 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" href="/css/style.css">
+    <title>
+        @isset($title)
+            {{ $title }}
+        @endisset
+    </title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <a class="navbar-brand logo" href="/home/">Home</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            @auth
+                <li class="nav-item">
+                    <a class="nav-link" href="/user/orders">Orders</a>
+                </li>
+            @endauth
+        </ul>
+        <ul class="navbar-nav mr-right">
+            <li class="nav-item">
+                <form action="/product/search" method="post" class="form-inline my-2 my-lg-0">
+                    <input name="keyword" class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                </form>
+            </li>
+            <li class="nav-item">
+                <a href="/cart/">
+                    <button type="button" class="btn btn-primary">
+                        <i class="fas fa-shopping-cart"></i>
+                        <span class="badge badge-light" id="products_number">
+                            @isset($productsNumber)
+                                {{ $productsNumber }}
+                            @else
+                                0
+                            @endisset
+                        </span>
+                    </button>
+                </a>
+            </li>
+            <li class="nav-item">
+                @auth
+                    <li> <a class="nav-link" href="/user/logout/" onclick="return confirmDelete();">Log out</a> </li>
+                @else
+                    <li> <a class="nav-link" href="/user/login/">Log in</a> </li>
+                @endauth
+            </li>
+        </ul>
+    </div>
+</nav>
+<div class="container">
+    @yield('content')
+</div>
+<script defer src="https://use.fontawesome.com/releases/v5.5.0/js/all.js" integrity="sha384-GqVMZRt5Gn7tB9D9q7ONtcp4gtHIUEW/yG7h98J7IpE3kpi+srfFyyB/04OV6pG0" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</body>
+</html>
