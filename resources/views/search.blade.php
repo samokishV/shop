7c31e84e3fe32f387896a1430b00ae11788b636d
Viktoriia Samokysh <viktoriia.samokysh@codeit.com.ua>
2019-05-28 16:56:17 +0300

    Non working link fixed on the searching page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,11 +10,11 @@
             <div class="card m-2" style="width: 18rem;">
                 <form method='post' action="/cart/add" class="products">
                     {{ csrf_field() }}
-                    <a href="/{{$product->slug}}">
+                    <a href="/product/{{$product->slug}}">
                         <img class="card-img-top" src="/{{$product->preview}}" alt="{{$product->title}}">
                     </a>
                     <div class="card-body">
-                        <h5 class="card-title bold"><a href="/{{$product->slug}}">{{$product->title}}</a></h5>
+                        <h5 class="card-title bold"><a href="/product/{{$product->slug}}">{{$product->title}}</a></h5>
                         <h5 class="card-title">{{$product->price}}</h5>
                         @if($product->in_stock > 0)
                             <h6 class="card-subtitle mb-2 text-muted">in stock: {{$product->in_stock}}</h6>
